670e1409f61e280972efd111a97c749f241c0ddb
santosh <santosh26.python@gmail.com>
2020-09-01 13:23:14 +0530

    created database and base.html edited

--- TestPro/testapp/models.py
+++ TestPro/testapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class User_Details(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    address = models.TextField()
+    user_email = models.EmailField()
+    profile_pic = models.ImageField()
 
+class Content(models.Model):
+    pass
--- TestPro/testapp/urls.py
+++ TestPro/testapp/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    # url(r'', ),
+    url(r'',test ),
 ]
 
 
